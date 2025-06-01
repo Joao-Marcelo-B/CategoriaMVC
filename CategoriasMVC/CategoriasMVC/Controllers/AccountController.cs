63be43abc88634aa590c72c9e4a67a2ab8ebd7a2
joao-marcelo <joao.narciso@wealthmoney.com.br>
2025-06-01 19:05:39 -0300

    test

--- CategoriasMVC/CategoriasMVC/Controllers/AccountController.cs
+++ CategoriasMVC/CategoriasMVC/Controllers/AccountController.cs
@@ -31,7 +31,7 @@ public class AccountController : Controller
         var result = await _autenticacao.AutenticaUsuario(usuarioViewModel);
         if (result is null)
         {
-            
+            return BadRequest("");
         }
     }
 }
