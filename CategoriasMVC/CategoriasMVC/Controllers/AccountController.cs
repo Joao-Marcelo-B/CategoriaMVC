159f576224a797e7d5012a65f1cd2f931706a18e
João Marcelo <joaomarcelobatistanarciso@gmail.com>
2025-06-01 14:44:35 -0300

    feat: endpoint para login

---
+++ CategoriasMVC/CategoriasMVC/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using CategoriasMVC.Models;
+using CategoriasMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CategoriasMVC.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly IAutenticacao _autenticacao;
+
+    public AccountController(IAutenticacao autenticacao)
+    {
+        _autenticacao = autenticacao;
+    }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(UsuarioViewModel usuarioViewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            ModelState.AddModelError(string.Empty, "Login Inválido...");
+            return View(usuarioViewModel);
+        }
+
+        var result = await _autenticacao.AutenticaUsuario(usuarioViewModel);
+        if (result is null)
+        {
+            
+        }
+    }
+}
